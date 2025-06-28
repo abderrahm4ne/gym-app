2acec72f975aa822b69e796cf8bb9c067393ec97
abderrahm4ne <abderrahmaneita@gmail.com>
2025-06-28 12:58:26 +0100

    app colors picks

--- gym-app/src/components/SideBar.jsx
+++ gym-app/src/components/SideBar.jsx
@@ -12,22 +12,22 @@ export default function SideBar(){
         <div className="h-[100vh] w-[100vw] flex flex-row">
             
 
-                <div className="flex flex-col w-[17%] bg-[#1f1e1e]" style={{borderRight:"3px #FFA673 solid"}}>
+                <div className="flex flex-col w-[17%]" style={{backgroundImage:"linear-gradient( #1a1919 40% , #1f1e1e 60%)", borderRight:"2px solid white"}}>
                     
-                        <div className="py-8 w-[100%] border-b-2 border-[#f75f07] bg-[#1f1e1e]"> 
-                            <p className="text-[#FFF287] text-xl text-center">GYM SYSTEM  MANAGEMENT</p>
+                        <div className="py-8 w-[100%] border-b-2 border-[#FFD8D8]"> 
+                            <p className="text-[#8CCDEB] text-xl text-center">GYM SYSTEM  MANAGEMENT</p>
                         </div>
 
-                        <div className="flex flex-row items-center justify-center gap-4 w-[100%] border-b-2 border-[#f75f07] py-8">
+                        <div className="flex flex-row items-center justify-center gap-4 w-[100%] border-b-2 border-[#FFD8D8] py-8">
 
                             <img src="" alt="pic" className="rounded-[50%] p-3 text-[#2b2a2a] bg-[#FFD8D8]" />
 
 
                             <div className="">
-                                <p className="text-2xl text-[#FFF287]">ADMIN SALIM</p>
+                                <p className="text-2xl text-[#8CCDEB]">ADMIN SALIM</p>
                             </div>
 
-                            <div className="rounded-[50%]" style={{border:"5px #33ab5d solid"}}> 
+                            <div className="rounded-[50%]" style={{border:"5px #07b335 solid"}}> 
 
                             </div>
 
@@ -35,12 +35,12 @@ export default function SideBar(){
                         
                         <div className="w-[100%] flex flex-col">
 
-                            <div className="py-5 pl-15 text-[#FFF287] text-2xl hover:bg-[black] hover:cursor-pointer flex flex-row gap-3.5">
+                            <div className="py-5 pl-15 text-[#8CCDEB] text-2xl hover:bg-[black] hover:cursor-pointer flex flex-row gap-3.5" >
                                 <DashboardIcon sx={{fontSize:"2rem"}}/>
                                 <NavLink to={"/"} >DASHBOARD</NavLink>
                             </div>
                             
-                            <div className="py-5 pl-15 text-[#FFF287] text-xl hover:bg-[black] hover:cursor-pointer flex flex-row gap-3.5">
+                            <div className="py-5 pl-15 text-[#8CCDEB] text-xl hover:bg-[black] hover:cursor-pointer flex flex-row gap-3.5">
                                 <GroupIcon sx={{fontSize:"2rem"}}/>
                                 <NavLink to={"/members"} >ALL MEMBERS</NavLink>
                             </div>
--- gym-app/src/components/routesComponents/Dashboard.jsx
+++ gym-app/src/components/routesComponents/Dashboard.jsx
@@ -1,20 +1,36 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import GroupIcon from '@mui/icons-material/Group';
+import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 
 export default function Dashboard(){
 
     return(
-        <div className="bg-[#efefef] h-[100vh]">
+        <div className="w-full h-[100vh] px-3 py-2 bg-[#383636]">
 
-            <div className="w-[100%] py-4 text-3xl pl-15 bg-[#2b2a2a] text-[#FFF287] flex flex-row items-center gap-3" style={{ boxShadow: "0px -2px 6px black", borderBottom:"3px solid #FFA673"}}>
+            <div style={{backgroundImage:"linear-gradient( #1a1919 40% , #1f1e1e 60%)", boxShadow:"0px 5px 10px #1f1e1e"}} className='text-3xl text-[#8CCDEB] bg-black py-5 px-6 rounded-lg'>
+                <h1>DASHBOARD</h1>
+            </div>
 
-                <DashboardIcon style={{fontSize:"2.5rem"}}/>
-                <p>DASHBOARD</p>
+            <div className='text-3xl text-[#8CCDEB] py-5 px-6 w-[14%]'>
+                <p style={{borderBottom:"2px solid #8CCDEB"}}>Gym Control</p>
             </div>
             
-            <div className='grid grid-cols-4 p-4'>
-                    <div className='bg-[#FFD8D8] p-6'>
-                        ADD MEMBER
-                    </div>
+            <div className='grid grid-cols-4 p-4 '>
+                    <div className='bg-[#1f1e1e] height-[30%] text-[#8CCDEB] flex flex-col items-center justify-center rounded-lg shadow-lg m-2 h-[280px] hover:cursor-pointer' style={{borderShadow:"20px 20px 20px white"}}>
+                        <GroupIcon sx={{fontSize:"10rem", height:"60%"}} />
+                        <div className='height-[40%]'>
+                            <p className='text-2xl'>VIEW ALL MEMBERS</p>
+                        </div>
+                        
+                  </div>
+
+                    <div className='bg-[#1f1e1e] height-[30%] text-[#8CCDEB] flex flex-col items-center justify-center rounded-lg shadow-lg m-2 h-[280px] hover:cursor-pointer'>
+                        <PersonAddAlt1Icon sx={{fontSize:"10rem", height:"60%"}} />
+                        <div className='height-[40%]'>
+                            <p className='text-2xl'>ADD NEW MEMBER</p>
+                        </div>
+                        
+                  </div>
             </div>
             
         </div>
