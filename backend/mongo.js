f8b0c7f439c0a2504bcee3ac11f792746e2572d3
abderrahm4ne <abderrahmaneita@gmail.com>
2025-07-04 16:11:46 +0100

    d

---
+++ backend/mongo.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const configPath = path.join(__dirname, "config.json");
+const config = JSON.parse(fs.readFileSync(configPath, "utf-8"));
+
+async function connectToDatabase() {
+  const uri = config.mongoUri;
+
+  if (!uri) {
+    console.error("Mongo URI is missing in config.json");
+    return;
+  }
+  console.log('uri is here ', uri);
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
+}
+
+export default connectToDatabase;
