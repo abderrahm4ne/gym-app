07d7c0f6fa7f2d8b2cd268d3699db3e5789b83f2
abderrahm4ne <abderrahmaneita@gmail.com>
2025-07-04 16:35:12 +0100

    sadsa

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import url from 'url';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { ipcMain } from 'electron';
-
+import { memberHandlers } from './handlers/memberHandlers.js';
 
 // apis
 import connectToDatabase from './backend/mongo.js';
@@ -37,6 +37,7 @@ function createMainWindow() {
 
 app.whenReady().then(async () => {
   await connectToDatabase();
+  memberHandlers();
   createMainWindow();
   
 });
