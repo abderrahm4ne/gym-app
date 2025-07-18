cc76f967c422fd27466dd3c42441fb20e30cc0ae
abderrahm4ne <abderrahmaneita@gmail.com>
2025-07-18 01:39:48 +0100

    start working on membership ends page

--- gym-app/src/Admin/MembershipsEndsToday.jsx
+++ gym-app/src/Admin/MembershipsEndsToday.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 
 export default function MembershipsEndsToday() {
+
+    const [members, setMembers] = useState([]);
+    const [error, setError] = useState(null);
+
+
+    const fetchMembers = async () => {
+
+        const result = await window.electron.ipcRenderer.invoke("get-members");
+            console.log(' fetched data : ', result)
+        if (result.success) {
+                setMembers(result.members);
+        } else {
+            setError(result.error);
+            console.error("Error fetching members:", result.error);
+        }
+    };
+
+    useEffect(() => {
+        fetchMembers();
+    }, []);
+
     return (
-        <>
-        Memberships ends today
-        </>
+        <div>
+            <h2>Memberships Ending Today</h2>
+            
+                    {members.map( member => {
+                       return member.firstname
+                    })}
+            
+        </div>
     )
 }
